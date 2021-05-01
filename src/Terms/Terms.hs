9a38c8b91a5b9fea66f577c8bf7f5df8841d1c9b
dii <dstephenhono@gmail.com>
2021-05-01 17:21:54 -0400

    Changing module structure.

---
+++ src/Terms/Terms.hs
@@ -0,0 +1,18 @@
+module Terms (
+    VName,
+    Term (V, T),
+    occurs
+) where
+
+type Sig = [String]
+
+type VName = (Char, Int)
+
+data Term 
+    = V VName 
+    | T String [Term] 
+    deriving (Eq,Show)
+
+occurs :: VName -> Term -> Bool 
+occurs x (V y) = x == y
+occurs x (T _ ts) = any (occurs x) ts 
