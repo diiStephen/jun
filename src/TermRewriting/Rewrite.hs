72ffc552788c2b9f342b18273c978bd78af46f18
dii <dstephenhono@gmail.com>
2021-05-04 16:05:58 -0400

    Fixing indentation.

--- src/TermRewriting/Rewrite.hs
+++ src/TermRewriting/Rewrite.hs
@@ -8,12 +8,12 @@ import Substitution.Substitutions
 rewrite :: (Term, Term) -> Term -> Maybe Term
 rewrite (l,r) s = case match' l s of 
                 Just sigma -> Just (applyLifted sigma r)
-                Nothing -> Nothing 
+                Nothing    -> Nothing 
 
 rewriteAll :: [(Term, Term)] -> Term -> Maybe Term 
 rewriteAll [] _ = Nothing
 rewriteAll (r:rs) s = case rewrite r s of
-                  Just t -> Just t 
+                  Just t  -> Just t 
                   Nothing -> rewriteAll rs s
 
 {-Potentially non-terminating-} 
@@ -21,7 +21,7 @@ normalize :: [(Term, Term)] -> Term -> Term
 normalize _ (V x) = V x
 normalize trs (T f ts) = let u = (T f (map (normalize trs) ts)) in 
                                case rewriteAll trs u of 
-                                    Just s -> normalize trs s 
+                                    Just s  -> normalize trs s 
                                     Nothing -> u
 
 {-Potentially non-terminating-}
@@ -33,5 +33,5 @@ normalize2 trs = go
         go (T f ts) = 
             let u = (T f (map go ts)) in 
                 case rewriteAll trs u of 
-                    Just s -> go s 
+                    Just s  -> go s 
                     Nothing -> u 
