405b810f2d41a3b5e8f1baac28c1e546f147c0c6
dii <dstephenhono@gmail.com>
2021-05-08 11:50:06 -0400

    M20210508-4: Adding further term parsing tests.

--- test/Terms/TermParserSpec.hs
+++ test/Terms/TermParserSpec.hs
@@ -9,6 +9,9 @@ import Terms.Terms
 
 main :: IO ()
 main = hspec $ do 
-    describe "The term parser topLevel function" $ do 
-        it "should parse a string representing a term" $ do 
+    describe "The term parser topLevel parser" $ do 
+        it "should parse a string representing the term f(x,y)" $ do 
             parse (topLevel ['f']) "f(x,y)" `shouldBe` [(T "f" [V ('x',1),V ('y',1)],"")] 
+        
+        it "should parse a string representing the term f(f(a,b),x)" $ do 
+            parse (topLevel ['f', 'a', 'b']) "f(f(a,b),x)" `shouldBe` [(T "f" [T "f" [T "a" [], T "b" []], V ('x',1)],"")]
