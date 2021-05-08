16e962d11d229c24b9f7ffc3cdaec46928132c07
dii <dstephenhono@gmail.com>
2021-05-08 11:36:08 -0400

    M20210508-4: Adding depdencies and setting up tests.

--- test/Terms/TermParserSpec.hs
+++ test/Terms/TermParserSpec.hs
@@ -1,5 +1,11 @@
-module TermParserSpec where 
+module Terms.TermParserSpec where 
 
 import Test.Hspec 
 import Test.QuickCheck 
 import Control.Exception (evaluate)
+
+main :: IO ()
+main = hspec $ do 
+    describe "Prelude.head" $ do 
+        it "returns the first element of a list" $ do 
+            head [23,24] `shouldBe` (23 :: Int)
