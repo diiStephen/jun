b33be3251b41b76e9c46dbe5520f1c05d6a783b1
dii <dstephenhono@gmail.com>
2021-05-08 18:57:55 -0400

    M20210508-4: Fixing unif spec descriptions.

--- test/Unification/UnificationSpec.hs
+++ test/Unification/UnificationSpec.hs
@@ -23,20 +23,20 @@ main = hspec $ do
             let unifProb = [(t3,t4), (t1,t2)]
             (unify unifProb []) `shouldBe` Just [(('y',1),T "f" [T "a" []]),(('x',1),T "f" [T "a" []])]
         
-        it "should produce Nothing for the equations {x =^ g(x)}" $ do 
+        it "should produce Nothing for the equations {x =^? g(x)}" $ do 
             let t1 = termGenerator ['g'] "g(x)"
             let t2 = termGenerator [] "x"
             let unifProb = [(t2, t1)]
             (unify unifProb []) `shouldBe` Nothing 
 
-        it "should produce the mgu for the equations {f(x,y) = f(f(a,b),x)}" $ do 
+        it "should produce the mgu for the equations {f(x,y) =^? f(f(a,b),x)}" $ do 
             let sig = ['f', 'a', 'b']
             let t1 = termGenerator sig "f(x,y)"
             let t2 = termGenerator sig "f(f(a,b),x)"
             let unifProb = [(t1,t2)] 
             (unify unifProb []) `shouldBe` Just [(('y',1),T "f" [T "a" [],T "b" []]),(('x',1),T "f" [T "a" [],T "b" []])] 
 
-        it "should produce Nothing for the equations {g(x) = f(y)}" $ do 
+        it "should produce Nothing for the equations {g(x) =^? f(y)}" $ do 
             let sig = ['f', 'g']
             let t1 = termGenerator sig "g(x)"
             let t2 = termGenerator sig "f(y)"
