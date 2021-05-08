d16784e0a734a7041ac2a81f170a36e438584e72
dii <dstephenhono@gmail.com>
2021-05-08 12:05:40 -0400

    M20210508-4: Adding variable parsing test.

--- test/Terms/TermParserSpec.hs
+++ test/Terms/TermParserSpec.hs
@@ -11,7 +11,10 @@ main :: IO ()
 main = hspec $ do 
     describe "The term parser topLevel parser" $ do 
         it "should parse a string representing the term f(x,y)" $ do 
-            parse (topLevel ['f']) "f(x,y)" `shouldBe` [(T "f" [V ('x',1),V ('y',1)],"")] 
+            parse (topLevel ['f']) "f(x,y)" `shouldBe` [(T "f" [V ('x',1),V ('y',1)], "")] 
         
         it "should parse a string representing the term f(f(a,b),x)" $ do 
-            parse (topLevel ['f', 'a', 'b']) "f(f(a,b),x)" `shouldBe` [(T "f" [T "f" [T "a" [], T "b" []], V ('x',1)],"")]
+            parse (topLevel ['f', 'a', 'b']) "f(f(a,b),x)" `shouldBe` [(T "f" [T "f" [T "a" [], T "b" []], V ('x',1)], "")]
+
+        it "should parse a string representing the term x" $ do 
+            parse (topLevel []) "x" `shouldBe` [(V ('x',1), "")]
