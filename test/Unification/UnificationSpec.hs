e55377680c8a2ff228867b0c9a01fecfdd575442
dii <dstephenhono@gmail.com>
2021-05-08 15:52:31 -0400

    M20210508-4: cleaning up unif tests.

--- test/Unification/UnificationSpec.hs
+++ test/Unification/UnificationSpec.hs
@@ -8,14 +8,17 @@ import Unification.Unification
 import Terms.TermParser
 import Terms.Terms 
 
+termGenerator :: [Char] -> String -> Term 
+termGenerator sig s = fst $ head $ parse (topLevel sig) s
+
 main :: IO () 
 main = hspec $ do 
     describe "The basic syntactic unification algorithm" $ do 
         it "should produce a correct mgu given the equations" $ do 
-            let t1 = fst $ head $ parse (topLevel ['g', 'a']) "g(x,x)"
-            let t2 = fst $ head $ parse (topLevel ['g', 'a']) "g(x,y)"
-            let t3 = fst $ head $ parse (topLevel ['g', 'a']) "x"
-            let t4 = fst $ head $ parse (topLevel ['f', 'a']) "f(a)"
+            let t1 = termGenerator ['g','a'] "g(x,x)"
+            let t2 = termGenerator ['g','a'] "g(x,y)"
+            let t3 = termGenerator ['g','a'] "x"
+            let t4 = termGenerator ['f','a'] "f(a)"
             let unifProb = [(t3,t4), (t1,t2)]
             (unify unifProb []) `shouldBe` Just [(('y',1),T "f" [T "a" []]),(('x',1),T "f" [T "a" []])]
             
