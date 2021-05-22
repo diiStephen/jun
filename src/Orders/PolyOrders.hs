e2c2cd93ed2a2ef68f0c059035d3bb294fdf1a4d
dii <dstephenhono@gmail.com>
2021-05-22 17:16:27 -0400

    M20210519: Generalizing lexOrd to Orderable type class.

--- src/Orders/PolyOrders.hs
+++ src/Orders/PolyOrders.hs
@@ -2,9 +2,12 @@ module Orders.PolyOrders where
 
 data Order = GR | E | NGE deriving (Show)
 
-lexOrd :: (a -> b -> Order) -> [a] -> [b] -> Order 
-lexOrd _ [] [] = E 
-lexOrd order (x:xs) (y:ys) = case order x y of 
+class Orderable a where 
+    order :: a -> a -> Order 
+
+lexOrd :: Orderable a => [a] -> [a] -> Order 
+lexOrd [] [] = E 
+lexOrd (x:xs) (y:ys) = case order x y of 
     GR -> GR 
-    E -> lexOrd order xs ys  
+    E -> lexOrd xs ys  
     NGE -> NGE  
