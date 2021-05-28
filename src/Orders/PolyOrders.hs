fe9f2d026019d8997b8da31aa74bc8c1d428870a
dii <dstephenhono@gmail.com>
2021-05-27 23:38:12 -0400

    M20210519: Adding type signature to local function verify.

--- src/Orders/PolyOrders.hs
+++ src/Orders/PolyOrders.hs
@@ -13,7 +13,7 @@ instance Orderable Int where
               | x > y  = GR
 
 lexOrd :: Orderable a => [a] -> [a] -> Order 
-lexOrd [] [] = E 
+lexOrd [] []         = E 
 lexOrd (x:xs) (y:ys) = case order x y of 
     GR  -> GR 
     E   -> lexOrd xs ys  
@@ -24,7 +24,8 @@ multiOrder :: Orderable a => [a] -> [a] -> Order
 multiOrder ms ns = if (null x) && (null y) then E else verify x y 
     where 
         x = multiSetMinus ns ms 
-        y = multiSetMinus ms ns 
+        y = multiSetMinus ms ns
+        verify :: Orderable a => [a] -> [a] -> Order 
         verify [] v     = GR
         verify (u:us) v = case find (\p -> (order p u) == GR) v of 
             Just t -> verify us v 
