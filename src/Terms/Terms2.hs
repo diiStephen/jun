c6db9499d62a83fe46fbde408da72f60c79862b9
dii <dstephenhono@gmail.com>
2021-06-09 18:07:50 -0400

    M20210519: Recording Terms2 for the next major revision. Fas and loose, but it works.

--- src/Terms/Terms2.hs
+++ src/Terms/Terms2.hs
@@ -2,16 +2,32 @@
 
 module Terms.Terms2 where 
 
+import Data.List (any, intercalate)
+
 type FSym       = String 
-type VName      = (String, Int)
 type OrderedSig = [FSym]
+
+data VName 
+    = VName {name :: String, index :: Int}
+    deriving (Eq)
 
 data Term a 
     = T { root :: FSym, subterms :: [Term a] }
     | V a
-    deriving (Show, Eq)
+    deriving (Eq)
+
+instance Show VName where 
+    show = name
+
+instance Show a => Show (Term a) where 
+    show (V x) = show x 
+    show t     = (root t) ++ "(" ++ (intercalate "," (map show (subterms t))) ++ ")" 
 
 instance Functor Term where 
     fmap :: (a -> b) -> Term a -> Term b 
     fmap f (V x) = V $ f x
     fmap f t     = T (root t) (fmap (fmap f) (subterms t))
+
+occurs :: Eq a => a -> Term a -> Bool 
+occurs x (V y) = x == y 
+occurs x t     = any (occurs x) (subterms t)
