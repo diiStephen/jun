b02c77c862ae41ecbcaa9b8d17fa852d59c2a187
dii <dstephenhono@gmail.com>
2021-06-09 23:29:50 -0400

    M20210519: Adding scoped type variables so that verify can have a signature.

--- src/Orders/PolyOrders.hs
+++ src/Orders/PolyOrders.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Orders.PolyOrders (
     Order(..),
     lexOrd,
@@ -16,13 +18,13 @@ lexOrd order (x:xs) (y:ys) = case order x y of
     NGE -> NGE
 
 -- M >_mul N <=> M \not = N /\ \forall n \in N - M. \exists m \in M - N . m > n 
-multiOrder :: (a -> a -> Order) -> [a] -> [a] -> Order 
+multiOrder :: forall a . (a -> a -> Order) -> [a] -> [a] -> Order 
 multiOrder order ms ns = if (null nMinusM) && (null mMinusN) 
     then E else verify order nMinusM mMinusN 
     where 
         nMinusM = multiSetMinus order ns ms 
         mMinusN = multiSetMinus order ms ns
-        --verify :: (a -> a -> Order) -> [a] -> [a] -> Order 
+        verify :: (a -> a -> Order) -> [a] -> [a] -> Order 
         verify order u v = if all (\n -> any (\m -> (order m n) == GR) mMinusN) nMinusM 
             then GR else NGE 
 
