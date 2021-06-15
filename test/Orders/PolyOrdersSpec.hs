a81244e0d288a0f08b8011c64423ae6333ad2226
dii <dstephenhono@gmail.com>
2021-06-15 17:38:47 -0400

    M20210519: Cleaning up unit tests for orders.

--- test/Orders/PolyOrdersSpec.hs
+++ test/Orders/PolyOrdersSpec.hs
@@ -1,11 +1,11 @@
 module Orders.PolyOrdersSpec where 
 
-import Test.Hspec 
-import Test.QuickCheck 
+import Test.Hspec        ( hspec, describe, it, shouldBe ) 
+import Test.QuickCheck   () 
 import Control.Exception (evaluate)
+import Orders.PolyOrders ( lexOrd, multiOrder, Order(NGE, GR, E) )
 
-import Orders.PolyOrders
-
+order :: Ord a => a -> a -> Order
 order x y | x == y = E 
           | x < y  = NGE 
           | x > y  = GR
@@ -17,30 +17,30 @@ main = hspec $ do
             it "should return GR on [1,1,2] >_lex [1,1,1]" $ do
                 let x = ([1,1,2] :: [Int]) 
                 let y = ([1,1,1] :: [Int])
-                (lexOrd order x y) `shouldBe` GR
+                lexOrd order x y `shouldBe` GR
 
             it "should return NGE on [1,1] >_lex [2,2]" $ do 
                 let x = ([1,1] :: [Int])
                 let y = ([2,2] :: [Int])
-                (lexOrd order x y) `shouldBe` NGE 
+                lexOrd order x y `shouldBe` NGE 
             
             it "should return E on [3,3,3] >_lex [3,3,3]" $ do 
                 let x = ([3,3,3] :: [Int])
                 let y = ([3,3,3] :: [Int])
-                (lexOrd order x y) `shouldBe` E 
+                lexOrd order x y `shouldBe` E 
         
         describe "The multiOrder implementation" $ do 
             it "should return GR on [5,3,1,1] >_mul [4,3,3,1]" $ do 
                 let x = ([5,3,1,1] :: [Int])
                 let y = ([4,3,3,1] :: [Int])
-                (multiOrder order x y) `shouldBe` GR 
+                multiOrder order x y `shouldBe` GR 
 
             it "should return E on [1] >_mul [1]" $ do 
                 let x = ([1] :: [Int])
                 let y = ([1] :: [Int])
-                (multiOrder order x y) `shouldBe` E
+                multiOrder order x y `shouldBe` E
             
             it "should return NGE on [6,4,4,1] >_mul [7,4,4,1]" $ do 
                 let x = ([6,4,4,1] :: [Int])
                 let y = ([7,4,4,1] :: [Int])
-                (multiOrder order x y) `shouldBe` NGE
+                multiOrder order x y `shouldBe` NGE
--- test/Orders/TermOrdersSpec.hs
+++ test/Orders/TermOrdersSpec.hs
@@ -1,18 +1,15 @@
 module Orders.TermOrdersSpec where 
 
-import Test.Hspec 
-import Test.QuickCheck 
+import Test.Hspec        ( hspec, describe, it, shouldBe ) 
+import Test.QuickCheck   () 
 import Control.Exception (evaluate)
-
-import Orders.PolyOrders
-import Orders.TermOrders
-import Terms.TermParser
-import Terms.Terms 
+import Orders.PolyOrders ( Order(GR) )
+import Orders.TermOrders ( lpo )
+import Terms.TermParser  ( parse, topLevel )
+import Terms.Terms       ( Term ) 
 
 getTerm :: [Char] -> String -> Term 
 getTerm sig s = fst $ head $ parse (topLevel sig) s
-
-
 
 main :: IO () 
 main = hspec $ do 
@@ -23,28 +20,28 @@ main = hspec $ do
                 let orderedSig = ["e", "f", "i"]
                 let s = getTerm sig "f(x,e)"
                 let t = getTerm sig "x"
-                (lpo orderedSig s t) `shouldBe` GR
+                lpo orderedSig s t `shouldBe` GR
             
             it "should return GR for i(e) > e" $ do
                 let sig = ['e', 'f', 'i']
                 let orderedSig = ["e", "f", "i"] 
                 let s = getTerm sig "i(e)"
                 let t = getTerm sig "e"
-                (lpo orderedSig s t) `shouldBe` GR 
+                lpo orderedSig s t `shouldBe` GR 
 
             it "should return GR for i(f(x,y)) > f(i(x), i(y))" $ do
                 let sig = ['e', 'f', 'i']
                 let orderedSig = ["e", "f", "i"]
                 let s = getTerm sig "i(f(x,y))"
                 let t = getTerm sig "f(i(x), i(y))"
-                (lpo orderedSig s t) `shouldBe` GR 
+                lpo orderedSig s t `shouldBe` GR 
 
             it "should return GR for f(f(x,y), z) > f(x, f(y,z))" $ do 
                 let sig = ['e', 'f', 'i']
                 let orderedSig = ["e", "f", "i"]
                 let s = getTerm sig "f(f(x,y), z)"
                 let t = getTerm sig "f(x,f(y,z))"
-                (lpo orderedSig s t) `shouldBe` GR  
+                lpo orderedSig s t `shouldBe` GR  
 
 
 
