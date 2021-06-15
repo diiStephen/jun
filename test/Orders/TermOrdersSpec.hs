4187af597b36d74e042674b4d4c6bd07efeb6657
dii <dstephenhono@gmail.com>
2021-06-15 18:29:46 -0400

    M20210519: Adding more tests to term orders.

--- test/Orders/TermOrdersSpec.hs
+++ test/Orders/TermOrdersSpec.hs
@@ -3,8 +3,8 @@ module Orders.TermOrdersSpec where
 import Test.Hspec        ( hspec, describe, it, shouldBe ) 
 import Test.QuickCheck   () 
 import Control.Exception (evaluate)
-import Orders.PolyOrders ( Order(GR) )
-import Orders.TermOrders ( lpo )
+import Orders.PolyOrders ( Order(GR, NGE) )
+import Orders.TermOrders ( lpo, mpo )
 import Terms.TermParser  ( parse, topLevel )
 import Terms.Terms       ( Term ) 
 
@@ -43,5 +43,32 @@ main = hspec $ do
                 let t = getTerm sig "f(x,f(y,z))"
                 lpo orderedSig s t `shouldBe` GR  
 
+        describe "The lpo function on the ordered signature g < s < f" $ do 
+            it "should return GR for f(s(x), s(y)) > f(g(g(x)), f(x, s(s(y))))" $ do 
+                let sig = ['g', 's', 'f']
+                let orderedSig = ["g", "s", "f"]
+                let s = getTerm sig "f(s(x), s(y))"
+                let t = getTerm sig "f(g(g(x)), f(x, s(s(y))))"
+                lpo orderedSig s t `shouldBe` GR
 
+            it "should return NGE for f(s(y), x) > s(f(x, s(s(y))))" $ do 
+                let sig = ['g', 's', 'f']
+                let orderedSig = ["g", "s", "f"]
+                let s = getTerm sig "f(s(y), x)"
+                let t = getTerm sig "s(f(x, s(s(y))))"
+                lpo orderedSig s t `shouldBe` NGE
 
+        describe "The mpo/lpo functions on the ordered signature f" $ do 
+            it "mpo should return NGE for f(f(x,y), z) > f(x, f(y,z))" $ do 
+                let sig = ['f']
+                let orderedSig = ["f"]
+                let s = getTerm sig "f(f(x,y), z)"
+                let t = getTerm sig "f(x, f(y,z))"
+                mpo orderedSig s t `shouldBe` NGE 
+
+            it "lpo should return GR for f(f(x,y), z) > f(x, f(y,z))" $ do 
+                let sig = ['f']
+                let orderedSig = ["f"]
+                let s = getTerm sig "f(f(x,y), z)"
+                let t = getTerm sig "f(x, f(y,z))"
+                lpo orderedSig s t `shouldBe` GR 
