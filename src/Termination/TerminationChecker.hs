df9bfc611630b8abe48643abb3161299312d00ff
dii <dstephenhono@gmail.com>
2021-06-17 23:20:57 -0400

    M20210519: checkLpo checkMpo termination functions.

--- src/Termination/TerminationChecker.hs
+++ src/Termination/TerminationChecker.hs
@@ -4,7 +4,8 @@ module Termination.TerminationChecker (
     RewriteSystem (Rules),
     lpoTactic,
     mpoTactic,
-    checkTermination,
+    checkLpoTermination,
+    checkMpoTermination,
     evalTermination
 ) where 
 
@@ -16,21 +17,19 @@ import Control.Monad.Identity ( Identity (runIdentity) )
 import Control.Monad.Except   ( ExceptT, throwError, MonadError (throwError), runExceptT )
 import Control.Monad.Reader   ( ReaderT, runReaderT, ask, asks )
 import Control.Monad.Writer   ( WriterT, runWriterT, tell )
+import Control.Monad          (mplus)
 
 newtype TerminationTactic = Tactic { runTactic :: Term -> Term -> Bool }
 data RewriteRule          = Rule { lhs :: Term, rhs :: Term }
 newtype RewriteSystem     = Rules { rules :: [RewriteRule] }
 
-data TerminationError  
-    = TFail String 
-    | OtherError String 
+newtype TerminationError = TFail String 
 
 type TerminationEval a = ReaderT OrderedSig (ExceptT TerminationError 
                                             (WriterT [String] Identity)) a
 
 instance Show TerminationError where 
     show (TFail s) = "Could not prove termination for rule: " ++ s 
-    show (OtherError s) = "Could not prove termination: " ++ s
 
 instance Show RewriteRule where 
     show r = show (lhs r) ++ " --> " ++ show (rhs r)
@@ -52,18 +51,21 @@ mpoTactic rule = do
     sig <- ask 
     case mpo sig (lhs rule) (rhs rule) of 
         GR -> return True 
-        _  -> throwError (TFail ("Rule" ++ show rule ++ "canot be proved termination with mpo"))
+        _  -> throwError (TFail ("Rule " ++ show rule ++ " cannot be proved terminating with mpo"))
 
 -- R is terminating iff \exists > , a reduction order on T(\Sigma, V) such that \forall l -> r \in R . l > r. 
 -- For any (partial) order on \Sigma the induced lpo/mpo a simplification order, and therefore a reduction order. 
 -- Note: The SAME reduction order must work for all rules in R. You cannot use differnt orders for each rule. 
 -- This is due to the fact that termination is NOT a modular property of TRS's.  
-checkTermination :: RewriteSystem -> TerminationEval Bool
-checkTermination rs = do
-    sig <- ask 
+checkLpoTermination :: RewriteSystem -> TerminationEval Bool
+checkLpoTermination rs = do
     lpoAttempt <- mapM lpoTactic (rules rs)
     return (and lpoAttempt)
 
+checkMpoTermination :: RewriteSystem -> TerminationEval Bool
+checkMpoTermination rs = do 
+    mpoAttempt <- mapM mpoTactic (rules rs)
+    return (and mpoAttempt)
 
-evalTermination :: OrderedSig -> RewriteSystem -> (Either TerminationError Bool, [String])
-evalTermination sig trs = runIdentity (runWriterT (runExceptT (runReaderT (checkTermination trs) sig)))
+evalTermination :: (RewriteSystem -> TerminationEval Bool) -> OrderedSig -> RewriteSystem -> (Either TerminationError Bool, [String])
+evalTermination strat sig trs = runIdentity (runWriterT (runExceptT (runReaderT (strat trs) sig)))
