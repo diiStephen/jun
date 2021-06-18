054e4d7e8887ab35071c92648f64ecf2420b9e0c
dii <dstephenhono@gmail.com>
2021-06-18 15:15:47 -0400

    M20210618: Adding CriticalPair type.

--- src/Confluence/CriticalPairs.hs
+++ src/Confluence/CriticalPairs.hs
@@ -1,1 +1,10 @@
-module Confluence.CriticalPairs () where
+module Confluence.CriticalPairs (
+    CriticalPair (..)
+) where
+
+import Terms.Terms ( Term )
+
+data CriticalPair = CP {left :: Term, right :: Term}
+
+instance Show CriticalPair where 
+    show (CP l r) = "〈 " ++ show l ++ "," ++ show r ++ " 〉"
