153dfec08ed2d83c159ef3b841e7284c4b03224d
dii <dstephenhono@gmail.com>
2021-06-18 16:40:48 -0400

    M20210618: Adding fpos helper function. Return all non-variable subterms of a term.

--- src/Confluence/CriticalPairs.hs
+++ src/Confluence/CriticalPairs.hs
@@ -1,10 +1,26 @@
 module Confluence.CriticalPairs (
     CriticalPair (..),
+    fpos
 ) where
 
-import Terms.Terms ( Term (..) )
+import Terms.Terms            ( Term (..) )
+import TermRewriting.Rewrite  ( RewriteSystem (..), RewriteRule (..) )
+import Control.Monad.Identity ( Identity )
+import Control.Monad.State    ( StateT )
+
 
 data CriticalPair = CP { left :: Term, right :: Term }
 
 instance Show CriticalPair where 
     show (CP l r) = "〈 " ++ show l ++ "," ++ show r ++ " 〉"
+
+type CriticalPairs = [CriticalPair]
+
+type Eval a = StateT CriticalPairs Identity a
+
+overlap :: RewriteRule -> RewriteRule -> Eval a
+overlap s t = undefined
+
+fpos :: Term -> [Term]
+fpos (V _)    = []
+fpos (T f ts) = (:) (T f ts) (ts >>= fpos) 
