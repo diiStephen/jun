2e7ccd79775cfe53c382157e361d9020d358d145
dii <dstephenhono@gmail.com>
2021-06-25 20:57:58 -0400

    M20210618: Adding criticalPair and criticalPairs function.

--- src/Confluence/CriticalPairs.hs
+++ src/Confluence/CriticalPairs.hs
@@ -1,9 +1,11 @@
 module Confluence.CriticalPairs (
     CriticalPair (..),
-    fpos
+    criticalPair,
+    criticalPairs,
+    allCriticalPairs
 ) where
 
-import Terms.Terms                ( Term (..), alphaConvert, maxIndex, subterms )
+import Terms.Terms                ( Term (..), alphaConvert, maxIndex, subterms, label, get, set, pos, label )
 import TermRewriting.Rewrite      ( RewriteSystem (..), RewriteRule (..) )
 import Unification.Unification    ( unify' )
 import Substitution.Substitutions (Subst, applyLifted)
@@ -14,22 +16,30 @@ import Control.Monad.State        ( StateT )
 data CriticalPair = CP { left :: Term, right :: Term }
 
 instance Show CriticalPair where 
-    show (CP l r) = "〈 " ++ show l ++ "," ++ show r ++ " 〉"
+    show (CP l r) = "〈 " ++ show l ++ " , " ++ show r ++ " 〉"
 
 type CriticalPairs = [CriticalPair]
 
 type Eval a = StateT CriticalPairs Identity a
 
--- s = l_1 --> r_1, t = l_2 --> r_2.
--- Try to unify (l_1)|p =^? l_2 , p \in FPos(l_1)
--- Critical Pair: (\theta(r_1), \theta(l_1)[\theta(r_2)]|p) 
---overlap :: Term - > Term -> Eval a
-overlap :: Term -> Term -> [Maybe Subst]
-overlap s t = map (unify' t) (fpos s) 
+mkDisjointVars :: RewriteRule -> RewriteRule -> RewriteRule
+mkDisjointVars rho tau = Rule variantLhs variantRhs
+    where
+        variantLhs = alphaConvert (maxIndex (lhs tau)) (lhs rho)
+        variantRhs = alphaConvert (maxIndex (rhs tau)) (rhs rho)
 
-criticalPair :: RewriteRule  -> RewriteRule -> CriticalPairs
-criticalPair rho tau = undefined 
+criticalPair :: RewriteRule -> RewriteRule -> String -> Maybe CriticalPair
+criticalPair r1 r2 p = case unify' (get (lhs r1) p) (lhs r2) of
+    Just sigma -> Just (CP (applyLifted sigma (rhs r1)) (set (applyLifted sigma (lhs r1)) (applyLifted sigma (rhs r2)) p))
+    Nothing    -> Nothing
 
-fpos ::Term -> [Term]
-fpos (V _)    = []
-fpos (T f ts) = (:) (T f ts) (ts >>= fpos)
+criticalPairs :: RewriteRule -> RewriteRule -> [Maybe CriticalPair]
+criticalPairs rho tau = criticalPair (mkDisjointVars rho tau) tau <$> nonVarPos
+    where nonVarPos = snd <$> filter (\(t,_) -> isNonVar t) (label (lhs rho))
+
+isNonVar :: Term -> Bool
+isNonVar (V _) = False 
+isNonVar (T _ _) = True
+
+allCriticalPairs :: RewriteSystem -> [[Maybe CriticalPair]]
+allCriticalPairs trs =  [criticalPairs rho tau | rho <- rules trs, tau <- rules trs]
