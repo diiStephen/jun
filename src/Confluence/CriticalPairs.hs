73cb0b04bd089a7f5b891c1f3ab0ee319b8e912c
dii <dstephenhono@gmail.com>
2021-06-25 23:28:09 -0400

    M20210618: cleaning up critical pairs module.

--- src/Confluence/CriticalPairs.hs
+++ src/Confluence/CriticalPairs.hs
@@ -11,9 +11,12 @@ import Unification.Unification    ( unify' )
 import Substitution.Substitutions (Subst, applyLifted)
 import Control.Monad.Identity     ( Identity )
 import Control.Monad.State        ( StateT )
+import Data.List                  ( union )    
+import Data.Maybe                 (isJust, fromJust)               
 
-
-data CriticalPair = CP { left :: Term, right :: Term }
+data CriticalPair 
+    = CP { left :: Term, right :: Term }
+    deriving (Eq)
 
 instance Show CriticalPair where 
     show (CP l r) = "〈 " ++ show l ++ " , " ++ show r ++ " 〉"
@@ -29,7 +32,7 @@ criticalPair r1 r2 p = case unify' (get (lhs r1) p) (lhs r2) of
 
 criticalPairs :: RewriteRule -> RewriteRule -> [Maybe CriticalPair]
 criticalPairs rho tau = criticalPair (mkDisjointVars rho tau) tau <$> nonVarPos
-    where nonVarPos = snd <$> filter (\(t,_) -> isNonVar t) (label (lhs rho))
+    where nonVarPos = snd <$> filter (isNonVar . fst) (label (lhs rho))
 
-allCriticalPairs :: RewriteSystem -> [[Maybe CriticalPair]]
-allCriticalPairs trs =  [criticalPairs rho tau | rho <- rules trs, tau <- rules trs]
+allCriticalPairs :: RewriteSystem -> [CriticalPair]
+allCriticalPairs trs = fromJust <$> filter isJust (foldr union [] [criticalPairs rho tau | rho <- rules trs, tau <- rules trs])
