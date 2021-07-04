da7f0557af3c3a28eb3e2064b04afa5c2cdd9478
dii <dstephenhono@gmail.com>
2021-07-04 15:45:43 -0400

    M20210704: Adding some ideas for basic completion.

--- src/Completion/BasicCompletion.hs
+++ src/Completion/BasicCompletion.hs
@@ -1,2 +1,18 @@
-module Completion.BasicCompletion ()
-where
+module Completion.BasicCompletion (
+    complete
+) where
+
+import TermRewriting.Rewrite    ( RewriteRule(..), RewriteSystem(..) )
+import Terms.Terms              ( OrderedSig, Term(..) )
+import Confluence.CriticalPairs ( CriticalPair(..) )
+
+data CompletionEnvironment 
+    = Env { 
+        symbolOrdering :: OrderedSig, 
+        criticalPairs :: [CriticalPair], 
+        rewriteSystem :: RewriteSystem, 
+        equations :: [(Term, Term)]
+    } deriving (Show)
+
+complete :: RewriteSystem -> Maybe RewriteSystem
+complete = undefined 
