86e066b31ba640899b707ec47e5a63fbc2c75d59
dii <dstephenhono@gmail.com>
2021-07-26 18:42:59 -0400

    M20210725: adding basic support for completion.

--- src/Interface/Repl.hs
+++ src/Interface/Repl.hs
@@ -4,19 +4,21 @@ module Interface.Repl (
     repl
 ) where
 
-import Terms.Terms             ( Term(..), OrderedSig )
-import Terms.TermParser        ( getTerm )
-import TermRewriting.Rewrite   ( RewriteSystem(..), RewriteRule(..) )
-import Equations.BasicEquation ( Equation(..), eqMap )
-import Control.Monad           ( void )
-import Control.Monad.RWS       ( RWST, liftIO, gets, runRWST, modify, get )
-import System.IO               ( hFlush, stdout ) 
+import Terms.Terms                ( Term(..), OrderedSig )
+import Terms.TermParser           ( getTerm )
+import TermRewriting.Rewrite      ( RewriteSystem(..), RewriteRule(..), mkRewriteSystem )
+import Equations.BasicEquation    ( Equation(..), eqMap )
+import Completion.HuetCompletion  (CompletionEnv(..), complete)
+import Orders.RecursivePathOrders ( lpo )
+import Control.Monad              ( void )
+import Control.Monad.RWS          ( RWST, liftIO, gets, runRWST, modify, get )
+import System.IO                  ( hFlush, stdout ) 
 
 type Log = [String]
 
 type ReplM a = (RWST String Log ReplEnv) IO a
 
-data ReplEnv = Env {
+data ReplEnv = REnv {
       signature :: OrderedSig
     , curRewriteSystem :: RewriteSystem
     , currRules :: [RewriteRule] 
@@ -27,19 +29,20 @@ defaultPrompt :: String
 defaultPrompt = ":> "
 
 commands :: [String]
-commands = ["exit", "add", "set-signature", "env"]
+commands = ["exit", "add", "signature", "env", "precedence", "kb"]
 
 repl :: IO ()
-repl = do 
+repl = do
+    liftIO $ putStr "Commands: " 
+    liftIO $ mapM_ (\s -> putStr (" " ++ s ++ " ")) commands >> putStrLn ""
     _ <- runRWST runRepl "term-rewriter" initReplEnv
-    putStrLn "Done."
+    return ()
 
 initReplEnv :: ReplEnv
-initReplEnv = Env {signature=[], curRewriteSystem = Rules [], currRules = [], curEquations = []}
+initReplEnv = REnv {signature=[], curRewriteSystem = Rules [], currRules = [], curEquations = []}
 
 runRepl :: ReplM ()
 runRepl = do
-    liftIO $ putStrLn $ "Basic Commands: " ++ show commands
     c <- liftIO $ prompt defaultPrompt
     let (com:args) = words c
     liftIO $ putStrLn $ "Command: " ++ com ++ " Args: " ++ show args
@@ -57,9 +60,11 @@ prompt p = do
 processCommand :: String -> [String] -> ReplM ()
 processCommand command args = do 
     case command of 
-        "add"           -> addEqn args
-        "set-signature" -> setSig args
-        "env"           -> showEnv
+        "add"        -> addEqn args
+        "signature"  -> setSig args
+        "env"        -> showEnv
+        "precedence" -> setSig args
+        "kb"         -> runKb args
         _ -> liftIO $ putStrLn "Command not found."  
 
 setSig :: [String] -> ReplM() 
@@ -82,3 +87,13 @@ showEnv :: ReplM ()
 showEnv = do 
     env <- get 
     liftIO $ print env 
+
+runKb :: [String] -> ReplM ()
+runKb args = do
+    eqsToComplete <- gets curEquations
+    sig <- gets signature 
+    let (result, complEnv, trace) = complete eqsToComplete (lpo sig)
+    liftIO $ mapM_ putStrLn trace
+    case result of 
+        Left _ -> return ()
+        Right () -> modify $ \env -> env { curRewriteSystem = mkRewriteSystem $ map snd (markedRules complEnv) }
