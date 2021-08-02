c1f248a7dc636ddc95967a854edd08b2a470f3b4
dii <dstephenhono@gmail.com>
2021-08-02 09:49:51 -0400

    M20210801: Adding string2Term functions.

--- src/StringRewriting/StringRewritingSystems.hs
+++ src/StringRewriting/StringRewritingSystems.hs
@@ -4,11 +4,30 @@
 module StringRewriting.StringRewritingSystems (
       StringRewriteRule(..)
     , Alphabet
+    , StringRewriteSystem
+    , stringToTerm
+    , stringToTerm2
 ) where
+
+import Terms.Terms    ( Term(..) )
+import Data.Bifunctor ( Bifunctor(..) )
 
 data StringRewriteRule a b where 
     (:->:) :: a -> b -> StringRewriteRule a b
     deriving (Eq)
 
+instance Bifunctor StringRewriteRule where 
+    bimap f g (s :->: t) = f s :->: g t
+
 type Alphabet = [String]
 
+type StringRewriteSystem a b = [StringRewriteRule a b]
+
+stringToTerm :: String -> Term 
+stringToTerm s = go (reverse s) 
+    where 
+        go [] = V ('x', 1)
+        go (c:cs) = T [c] [go cs]
+
+stringToTerm2 :: Foldable t => t Char -> Term
+stringToTerm2 = foldl (\t c -> T [c] [t]) (V ('x',1))
