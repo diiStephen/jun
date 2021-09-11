578270655c7b0e1b646178fdd8a8ff718aeb26fa
Daniel <dstephenhono@gmail.com>
2021-09-11 15:30:56 -0400

    M20210826: Adding inillij files to ignore.

--- test/ForwardClosure/ClosureSpec.hs
+++ test/ForwardClosure/ClosureSpec.hs
@@ -12,10 +12,10 @@ spec = do
     describe "the computeFowardClosure function" $ do
       it "should compute the forward closure of the example term rewriting system" $ do
         let sig = ["f", "s"]
-        let rho1 = Rule (getTerm sig "f(s(x))") (getTerm sig "s(x)")
+        let rho1 = Rule (getTerm sig "f(s(x))") (getTerm sig "f(x)")
         let rho2 = Rule (getTerm sig "s(s(s(x)))") (getTerm sig "x")
         let ex = Rules [rho1, rho2]
         let result = computeForwardClosure 2 ex
         let resultSize = length (rules result)
-        resultSize `shouldBe` 2
+        resultSize `shouldBe` 3
         print result
